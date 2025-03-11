43b68623eade1f6e9ef916a3a094103a90cecb97
BeatrizMariaPessoa <pessoa.beatriz@escolar.ifrn.edu.br>
2025-03-11 12:14:17 -0300

    app inicial

---
+++ src/Titulo.tsx
@@ -0,0 +1,12 @@
+import { useState } from "react";
+
+
+function Titulo({cor}) {
+    const [texto, setTexto] = useState("Um titulo qualquer... :(")
+    return <div>
+            <h1 style={{color : cor}}>{texto}</h1>
+            <button style={{backgroundColor : cor, color: "white"}} onClick={() => setTexto("Um titulo especial! :)")}>Mudar texto</button>
+        </div>
+}
+
+export default Titulo
