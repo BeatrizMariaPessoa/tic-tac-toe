ad3ebb337898f35c7ba242637aeaa047f2784590
BeatrizMariaPessoa <pessoa.beatriz@escolar.ifrn.edu.br>
2025-03-11 15:29:40 -0300

    resolvendo erros

--- src/Titulo.tsx
+++ src/Titulo.tsx
@@ -1,7 +1,10 @@
 import { useState } from "react";
 
+interface TituloProps {
+    cor: string;
+}
     
-function Titulo({cor}) {
+function Titulo({ cor }: TituloProps) {
     const [texto, setTexto] = useState("Um titulo qualquer... :(")
     return <div>
             <h1 style={{color : cor}}>{texto}</h1>
